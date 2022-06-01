5ea708f7fe59bd316cb8df7c4de3cdff5741cc53
bekatan <bekatans@gmail.com>
2022-06-02 08:47:18 +0900

    We still here!!! Dimensionality problems

--- mymlp.py
+++ mymlp.py
@@ -1,98 +1,77 @@
 import numpy as np
 import pandas as pd
-import math
 
-headers = ["x1", "x2", "y"]
-Trn = pd.read_csv("Trn.csv", names = headers)
-
-class Layer:
-    
-    #A building block. Each layer is capable of performing two things:    
-    # #- Process input to get output:           
-    # output = layer.forward(input)
-    
-    #- Propagate gradients through itself:    
-    # grad_input = layer.backward(input, grad_output)
-    
-    #Some layers also have learnable parameters which they update during layer.backward.
-    
-    def __init__(self):
-        # Here we can initialize layer parameters (if any) and auxiliary stuff.
-        # A dummy layer does nothing
-        pass
-    
-    def forward(self, input):
-        # Takes input data of shape [batch, input_units], returns output data [batch, output_units]
-        
-        # A dummy layer just returns whatever it gets as input.
-        return input    
-    
-    def backward(self, input, grad_output):
-        # Performs a backpropagation step through the layer, with respect to the given input.
-        
-        # To compute loss gradients w.r.t input, we need to apply chain rule (backprop):
-        
-        # d loss / d x  = (d loss / d layer) * (d layer / d x)
-        
-        # Luckily, we already receive d loss / d layer as input, so you only need to multiply it by d layer / d x.
-        
-        # If our layer has parameters (e.g. dense layer), we also need to update them here using d loss / d layer
-        
-        # The gradient of a dummy layer is precisely grad_output, but we'll write it more explicitly
-        num_units = input.shape[1]
-        
-        d_layer_d_input = np.eye(num_units)
-        
-        return np.dot(grad_output, d_layer_d_input) # chain rule
-
-class Sigmoid(Layer):
-    def __init__(self):
-        # ReLU layer simply applies elementwise rectified linear unit to all inputs
-        pass
-    
-    def forward(self, input):
-        # Apply elementwise ReLU to [batch, input_units] matrix
-        sigmoid_forward = (1 - math.exp(-input))**(-1)
-        return relu_forward
-    
-    def backward(self, input, grad_output):
-        # Compute gradient of loss w.r.t. ReLU input
-        relu_grad = input > 0
-        return grad_output*relu_grad
-
-class Dense(Layer):
+class Hidden:
     def __init__(self, input_units, output_units, learning_rate=0.1):
-        # A dense layer is a layer which performs a learned affine transformation:
-        # f(x) = <W*x> + b
         
         self.learning_rate = learning_rate
-        self.weights = np.random.normal(
-                                        scale = np.sqrt(2/(input_units+output_units)), 
-                                        size = (input_units,output_units))
-        self.biases = np.zeros(output_units)
+        self.weights = np.random.rand(input_units,output_units)
+        self.offset = np.random.rand(output_units)
         
     def forward(self,input):
-        # Perform an affine transformation:
-        # f(x) = <W*x> + b
         
-        # input shape: [batch, input_units]
-        # output shape: [batch, output units]
-        
-        return np.dot(input,self.weights) + self.biases
+        h_forward = np.dot(input,self.weights) + self.offset
+        sigmoid_forward = 1 / (1 + np.exp(-h_forward))
+        #print(sigmoid_forward)
+        return sigmoid_forward
     
-    def backward(self,input,grad_output):
-        # compute d f / d x = d f / d dense * d dense / d x
-        # where d dense/ d x = weights transposed
-        grad_input = np.dot(grad_output, self.weights.T)
+    def backward(self,input, output, error):
+        sigmoid_backward = output * (1.0 - output)
+        error = np.dot(error, self.weights.T)
+        delta = error * sigmoid_backward
         
-        # compute gradient w.r.t. weights and biases
-        grad_weights = np.dot(input.T, grad_output)
-        grad_biases = grad_output.mean(axis=0)*input.shape[0]
+        grad_weights = np.dot(input, delta)
         
-        assert grad_weights.shape == self.weights.shape and grad_biases.shape == self.biases.shape
-        
-        # Here we perform a stochastic gradient descent step. 
         self.weights = self.weights - self.learning_rate * grad_weights
-        self.biases = self.biases - self.learning_rate * grad_biases
+        self.offset = self.offset - self.learning_rate * error 
         
-        return grad_input
+        return delta
+
+headers = ["x1", "x2", "y"]
+df = pd.read_csv("Trn.csv", names = headers)
+
+X_train  = df.drop(["y"], axis=1).to_numpy(dtype = np.float128)[:10]
+Y_train  = df["y"].to_numpy(dtype=int)
+
+mlp = []
+mlp.append(Hidden(2, 5, learning_rate=1))
+mlp.append(Hidden(5, 1, learning_rate=1))
+
+def forward(mlp, X):
+    
+    activations = []
+    input = X
+    for l in mlp:
+        activations.append(l.forward(input))
+        input = activations[-1]
+        
+    assert len(activations) == len(mlp)
+    
+    return activations
+    
+def train(network,X,y,index):
+    layer_activations = forward(network,X)
+    layer_inputs = [X] + layer_activations
+    h_i_3 = layer_activations[-1]
+    
+    e_i = h_i_3[index] - y[index]
+
+    for layer_index in reversed(range(len(network)-1)):
+        layer = network[layer_index]
+        e_i = layer.backward(layer_inputs[layer_index],layer_inputs[layer_index+1],e_i)
+        
+    return np.mean(e_i)        
+
+train_log = []
+
+indices = np.random.permutation(len(X_train))
+
+for epoch in range(25):    
+    for index in indices:
+        train(mlp,X_train,Y_train, index)
+    
+    pr = (forward(mlp,X_train)[-1])
+    train_log.append(np.mean(pr==Y_train))
+    
+    print("Epoch",epoch)
+    print("Train accuracy:",train_log[-1])
